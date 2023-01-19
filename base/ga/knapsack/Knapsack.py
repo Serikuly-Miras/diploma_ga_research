2191b2f95a79bc467ef2eda0182de82f288d6085
Miras Serikuly <Serikuly.miras.16@gmail.com>
2023-01-20 01:09:13 +0300

    Improved knapsack solution

--- base/ga/knapsack/Knapsack.py
+++ base/ga/knapsack/Knapsack.py
@@ -1,121 +1,26 @@
-from dataclasses import dataclass
 import numpy as np
 
-
-@dataclass
-class KnapsackSettings:
-    population_size: int
-    num_objects: int
-    iterations: int
-    profits: np.ndarray
-    weights: np.ndarray
-    capacity: int
-    crossover_rate: float
-    mutation_rate: float
-    elitism_num: float
+from KnapsackTaichi import KnapsackSettings
 
 
 class Knapsack:
     def __init__(self, settings: KnapsackSettings):
-
+        self.s = settings
         pop_shape = (settings.population_size, settings.num_objects)
         self.population = np.full(dtype=np.int8, shape=pop_shape, fill_value=np.random.randint(
             2, size=pop_shape))
-        self.selection_pool = np.ndarray(dtype=np.int8, shape=pop_shape)
         self.new_gen = np.ndarray(dtype=np.int8, shape=pop_shape)
-
+        self.selection_pool = np.ndarray(
+            dtype=int, shape=settings.population_size)
         self.fitness = np.ndarray(
             dtype=np.int32, shape=settings.population_size)
-        self.history = np.ndarray(dtype=np.int32, shape=settings.iterations)
+        self.best = np.ndarray(dtype=np.int32, shape=settings.iterations)
+        self.avg = np.ndarray(dtype=np.int32, shape=settings.iterations)
 
-        self.population_size = settings.population_size
-        self.num_objects = settings.num_objects
-        self.iterations = settings.iterations
-        self.profits = settings.profits
-        self.weights = settings.weights
-        self.capacity = settings.capacity
-        self.crossover_rate = settings.crossover_rate
-        self.mutation_rate = settings.mutation_rate
-        self.elitism_num = settings.elitism_num
         self.epoch = 0
 
-    def best_fit(self) -> int:
-        max = 0
-        for i in range(self.population_size):
-            if max < self.fitness[i]:
-                max = self.fitness[i]
-        return max
-
-    def avg_fit(self) -> int:
-        sum = 0
-        for i in range(self.population_size):
-            sum += self.fitness[i]
-        return sum / self.population_size
-
-    def calc_fitness(self):
-        for i in range(self.population_size):
-            fit = 0
-            weight = 0
-            for j in range(self.num_objects):
-                if self.population[i, j] == 1:
-                    fit += self.profits[j]
-                    weight += self.weights[j]
-
-            if weight > self.capacity:
-                fit = 0
-
-            self.fitness[i] = fit
-
-    def tornament_selection(self):
-        for i in range(self.population_size):
-            left = np.random.randint(self.population_size)
-            right = np.random.randint(self.population_size)
-            res = left if self.fitness[left] > self.fitness[right] else right
-            for j in range(self.num_objects):
-                self.selection_pool[i, j] = self.population[res, j]
-
-    def crossover(self):
-        for i in range(int(self.population_size // 2 * self.crossover_rate)):
-            split = np.random.randint(self.num_objects)
-            left, right = 0, 0
-            for j in range(self.num_objects):
-                if (j < split):
-                    left = self.selection_pool[2 * i, j]
-                    right = self.selection_pool[2 * i + 1, j]
-                else:
-                    right = self.selection_pool[2 * i, j]
-                    left = self.selection_pool[2 * i + 1, j]
-
-                self.new_gen[2 * i, j] = left
-                self.new_gen[2 * i + 1, j] = right
-
-    def mutation(self):
-        for i in range(int(self.mutation_rate * self.population_size * self.num_objects)):
-            c = np.random.randint(self.population_size)         # creature
-            t = np.random.randint(self.num_objects)             # triplet
-            self.new_gen[c, t] = 1 - self.new_gen[c, t]         # flipping bit
-
-    def elitism(self):
-        best = 0
-        for i in range(self.population_size):
-            if self.fitness[best] < self.fitness[i]:
-                best = i
-
-        for i in range(self.elitism_num):
-            place = np.random.randint(self.population_size)
-            for j in range(self.num_objects):
-                self.new_gen[place, j] = self.population[best, j]
-
-    def log(self):
-        self.history[self.epoch] = self.best_fit()
-
-    def switch_population(self):
-        for i in range(self.population_size):
-            for j in range(self.num_objects):
-                self.population[i, j] = self.new_gen[i, j]
-
     def advance(self):
-        while(self.epoch < self.iterations):
+        while (self.epoch < self.s.iterations):
             self.calc_fitness()
             self.tornament_selection()
             self.crossover()
@@ -125,3 +30,81 @@ class Knapsack:
             self.log()
             self.switch_population()
             self.epoch += 1
+
+    def calc_fitness(self):
+        for i in range(self.s.population_size):
+            fit = 0
+            weight = 0
+            for j in range(self.s.num_objects):
+                if self.population[i, j] == 1:
+                    fit += self.s.profits[j]
+                    weight += self.s.weights[j]
+
+            if weight > self.s.capacity:
+                fit = 0
+
+            self.fitness[i] = fit
+
+    def tornament_selection(self):
+        for i in range(self.s.population_size):
+            left = np.random.randint(self.s.population_size)
+            right = np.random.randint(self.s.population_size)
+            res = left if self.fitness[left] > self.fitness[right] else right
+            self.selection_pool[i] = res
+
+    def crossover(self):
+        for i in range(self.s.population_size // 2):
+            split = np.random.randint(self.s.num_objects)
+            left, right = 0, 0
+            l = self.selection_pool[2 * i]
+            r = self.selection_pool[2 * i + 1]
+
+            for j in range(self.s.num_objects):
+                if (j < split):
+                    left = self.population[l, j]
+                    right = self.population[r, j]
+                else:
+                    left = self.population[r, j]
+                    right = self.population[l, j]
+
+                self.new_gen[2 * i, j] = left
+                self.new_gen[2 * i + 1, j] = right
+
+    def mutation(self):
+        for i in range(int(self.s.mutation_rate * self.s.population_size)):
+            c = np.random.randint(self.s.population_size)         # creature
+            t = np.random.randint(self.s.num_objects)             # triplet
+            self.new_gen[c, t] = 1 - self.new_gen[c, t]           # mutate
+
+    def elitism(self):
+        best = 0
+        for i in range(self.s.population_size):
+            if self.fitness[best] < self.fitness[i]:
+                best = i
+
+        for i in range(self.s.elitism_num):
+            place = np.random.randint(self.s.population_size)
+            for j in range(self.s.num_objects):
+                self.new_gen[place, j] = self.population[best, j]
+
+    def switch_population(self):
+        for i in range(self.s.population_size):
+            for j in range(self.s.num_objects):
+                self.population[i, j] = self.new_gen[i, j]
+
+    def log(self):
+        self.best[self.epoch] = self.best_fit()
+        self.avg[self.epoch] = self.avg_fit()
+
+    def best_fit(self) -> int:
+        b = 0
+        for i in range(self.s.population_size):
+            if b < self.fitness[i]:
+                b = self.fitness[i]
+        return b
+
+    def avg_fit(self) -> float:
+        s = 0.0
+        for i in range(self.s.population_size):
+            s += self.fitness[i]
+        return s / self.s.population_size
--- base/ga/knapsack/KnapsackTaichi.py
+++ base/ga/knapsack/KnapsackTaichi.py
@@ -1,7 +1,20 @@
 import numpy as np
 import taichi as ti
 
-from Knapsack import KnapsackSettings
+from dataclasses import dataclass
+
+
+@dataclass
+class KnapsackSettings:
+    population_size: int
+    num_objects: int
+    iterations: int
+    profits: np.ndarray
+    weights: np.ndarray
+    capacity: int
+    mutation_rate: float
+    elitism_num: float
+
 
 @ti.data_oriented
 class KnapsackTaichi:
@@ -11,27 +24,22 @@ class KnapsackTaichi:
         ti.init(arch=architecture, kernel_profiler=True, default_ip=ti.i32, default_fp=ti.f32,
                 cpu_max_num_threads=max_num_threads)
 
+        self.s = settings
         pop_shape = (settings.population_size, settings.num_objects)
-        self.population = ti.field(ti.u8, pop_shape, order='ij')
-        self.selection_pool = ti.field(ti.u8, pop_shape, order='ij')
-        self.new_gen = ti.field(ti.u8, pop_shape, order='ij')
+        self.population = ti.field(ti.u8, pop_shape)
+        self.new_gen = ti.field(ti.u8, pop_shape)
 
-        self.fitness = ti.field(int, (settings.population_size))
-        self.history = ti.field(int, (settings.iterations))
+        self.selection_pool = ti.field(ti.u32, settings.population_size)
+
+        self.fitness = ti.field(ti.u32, (settings.population_size))
+        self.best = ti.field(ti.u32, (settings.iterations))
+        self.avg = ti.field(ti.f64, (settings.iterations))
         self.epoch = ti.field(int, (1))
 
         self.profits = ti.field(int, settings.num_objects)
         self.weights = ti.field(int, settings.num_objects)
 
-        self.population_size = settings.population_size
-        self.num_objects = settings.num_objects
-        self.iterations = settings.iterations
-        self.capacity = settings.capacity
-        self.crossover_rate = settings.crossover_rate
-        self.mutation_rate = settings.mutation_rate
-        self.elitism_num = settings.elitism_num
-
-        # transfer profits and weights into taichi filds
+        # transfer profits and weights into taichi fields
         for i in range(settings.num_objects):
             self.profits[i] = settings.profits[i]
 
@@ -43,103 +51,104 @@ class KnapsackTaichi:
 
     @ti.kernel
     def randomize_population(self):
-        for i in range(self.population_size):
-            for j in range(self.num_objects):
-                self.population[i, j] = ti.random(int) % 2
+        for i, j in self.population:
+            self.population[i, j] = ti.random(int) % 2
 
-    @ti.func
-    def best_fit(self) -> int:
-        max = 0
-        for i in range(self.population_size):
-            if max < self.fitness[i]:
-                max = self.fitness[i]
-        return max
-
-    @ti.func
-    def avg_fit(self) -> int:
-        sum = 0
-        for i in range(self.population_size):
-            sum += self.fitness[i]
-        return sum / self.population_size
+    @ti.kernel
+    def advance(self):
+        while (self.epoch[0] < self.s.iterations):
+            self.calc_fitness()
+            self.tornament_selection()
+            self.crossover()
+            self.mutation()
+            self.elitism()
+            self.log()
+            self.switch_population()
+            self.epoch[0] += 1
 
     @ti.func
     def calc_fitness(self):
-        for i in range(self.population_size):
+        for i in range(self.s.population_size):
             fit = 0
             weight = 0
-            for j in range(self.num_objects):
+            for j in range(self.s.num_objects):
                 if self.population[i, j] == 1:
                     fit += self.profits[j]
                     weight += self.weights[j]
 
-            if weight > self.capacity:
+            if weight > self.s.capacity:
                 fit = 0
 
             self.fitness[i] = fit
 
     @ti.func
     def tornament_selection(self):
-        for i in range(self.population_size):
-            left = ti.random(int) % self.population_size
-            right = ti.random(int) % self.population_size
+        for i in range(self.s.population_size):
+            left = ti.random(int) % self.s.population_size
+            right = ti.random(int) % self.s.population_size
             res = left if self.fitness[left] > self.fitness[right] else right
-            for j in range(self.num_objects):
-                self.selection_pool[i, j] = self.population[res, j]
+            self.selection_pool[i] = res
 
     @ti.func
     def crossover(self):
-        for i in range(int(self.population_size // 2 * self.crossover_rate)):
-            split = int(ti.random(float) * self.num_objects)
+        for i in range(self.s.population_size // 2):
+            split = int(ti.random(float) * self.s.num_objects)
             left, right = 0, 0
-            for j in range(self.num_objects):
+            l = self.selection_pool[2 * i]
+            r = self.selection_pool[2 * i + 1]
+
+            for j in range(self.s.num_objects):
                 if (j < split):
-                    left = self.selection_pool[2 * i, j]
-                    right = self.selection_pool[2 * i + 1, j]
+                    left = self.population[l, j]
+                    right = self.population[r, j]
                 else:
-                    right = self.selection_pool[2 * i, j]
-                    left = self.selection_pool[2 * i + 1, j]
+                    left = self.population[r, j]
+                    right = self.population[l, j]
 
                 self.new_gen[2 * i, j] = left
                 self.new_gen[2 * i + 1, j] = right
 
     @ti.func
     def mutation(self):
-        for i in range(int(self.mutation_rate * self.population_size * self.num_objects)):
-            c = ti.random(int) % self.population_size                # creature
-            t = ti.random(int) % self.num_objects                    # triplet
-            self.new_gen[c, t] = 1 - self.new_gen[c, t]         # flipping bit
+        for i in range(int(self.s.mutation_rate * self.s.population_size)):
+            c = ti.random(int) % self.s.population_size         # creature
+            t = ti.random(int) % self.s.num_objects             # triplet
+            self.new_gen[c, t] = 1 - self.new_gen[c, t]         # mutate
 
     @ti.func
     def elitism(self):
         best = 0
-        for i in range(self.population_size):
+        for i in range(self.s.population_size):
             if self.fitness[best] < self.fitness[i]:
                 best = i
 
-        for i in range(self.elitism_num):
-            place = int(ti.random(float) * self.population_size)
-            for j in range(self.num_objects):
+        for i in range(self.s.elitism_num):
+            place = int(ti.random(float) * self.s.population_size)
+            for j in range(self.s.num_objects):
                 self.new_gen[place, j] = self.population[best, j]
 
     @ti.func
     def log(self):
-        self.history[self.epoch[0]] = self.best_fit()
+        self.best[self.epoch[0]] = self.best_fit()
+        self.avg[self.epoch[0]] = self.avg_fit()
 
     @ti.func
     def switch_population(self):
-        for i in range(self.population_size):
-            for j in range(self.num_objects):
+        for i in range(self.s.population_size):
+            for j in range(self.s.num_objects):
                 self.population[i, j] = self.new_gen[i, j]
 
-    @ti.kernel
-    def advance(self):
-        while(self.epoch[0] < self.iterations):
-            self.calc_fitness()
-            self.tornament_selection()
-            self.crossover()
-            self.mutation()
-            self.elitism()
+    @ti.func
+    def best_fit(self) -> int:
+        b = 0
+        for i in range(self.s.population_size):
+            if b < self.fitness[i]:
+                b = self.fitness[i]
+        return b
 
-            self.log()
-            self.switch_population()
-            self.epoch[0] += 1
+    @ti.func
+    def avg_fit(self) -> float:
+        s = 0.0
+        for i in range(self.s.population_size):
+            s += self.fitness[i]
+        return s / self.s.population_size
